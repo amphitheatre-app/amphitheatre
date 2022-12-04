dd74bab4429a74013105587da4b866b8ac4e4132
Eguo Wang <wangeguo@gmail.com>
2022-12-05 00:01:39 +0800

    Enabled rate limit by tower-governor crate

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,44 @@
 
 #![allow(unused_variables)]
 
+use std::net::SocketAddr;
+
 use amphitheatre::database::Database;
 use amphitheatre::routes;
-use axum::Extension;
+use axum::error_handling::HandleErrorLayer;
+use axum::http::StatusCode;
+use axum::{BoxError, Extension};
+use tower::ServiceBuilder;
+use tower_governor::governor::GovernorConfigBuilder;
+use tower_governor::GovernorLayer;
 
 #[tokio::main]
 async fn main() {
     let database = Database::new();
 
+    let governor_conf = GovernorConfigBuilder::default()
+        .per_second(1024)
+        .burst_size(1024)
+        .use_headers()
+        .finish()
+        .unwrap();
     // build our application with a single route
-    let app = routes::build().layer(Extension(database));
+    let app = routes::build().layer(Extension(database)).layer(
+        ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled error: {}", err),
+                )
+            }))
+            .layer(GovernorLayer {
+                config: &governor_conf,
+            }),
+    );
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
 }
