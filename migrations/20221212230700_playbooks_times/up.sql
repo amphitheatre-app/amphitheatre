72af27e149889295dbc42cee2461f572543f3f05
Eguo Wang <wangeguo@gmail.com>
2022-12-17 20:41:22 +0800

    fixed alter playbooks updated_at field sql error

--- migrations/20221212230700_playbooks_times/up.sql
+++ migrations/20221212230700_playbooks_times/up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `playbooks` ADD `created_at` DATETIME  NULL  DEFAULT now();
-ALTER TABLE `playbooks` ADD `updated_at` DATETIME  NULL  DEFAULT now()  AFTER `created_at`;
+ALTER TABLE `playbooks` ADD `created_at` DATETIME  NULL  DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `playbooks` ADD `updated_at` DATETIME  NULL  DEFAULT CURRENT_TIMESTAMP;
