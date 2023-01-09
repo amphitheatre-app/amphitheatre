6f741177a8c2307b1c6ee7b60ada568ed2c7fc33
Eguo Wang <wangeguo@gmail.com>
2023-01-09 10:06:17 +0800

    Update the Deployment resource definition used by the deployment::create() method

--- src/resources/deployment.rs
+++ src/resources/deployment.rs
@@ -14,7 +14,7 @@
 
 use k8s_openapi::api::apps::v1::Deployment;
 use kube::api::PostParams;
-use kube::{Api, Client};
+use kube::{Api, Client, ResourceExt};
 use serde_json::{from_value, json};
 
 use super::error::Result;
@@ -26,14 +26,42 @@ pub async fn create(client: Client, playbook: &Playbook, actor: &Actor) -> Resul
     let params = PostParams::default();
 
     let mut deployment = from_value(json!({
-        "apiVersion": "v1",
-        "kind": "Deployment",
-        "metadata": {
-            "name": actor.name,
-            "labels": {
-                "app.kubernetes.io/managed-by": "Amphitheatre"
-            }
+      "apiVersion": "apps/v1",
+      "kind": "Deployment",
+      "metadata": {
+        "name": actor.name,
+        "labels": {
+          "app.kubernetes.io/name": actor.name,
+          "app.kubernetes.io/instance": playbook.name_any(),
+          "app.kubernetes.io/managed-by": "Amphitheatre"
         }
+      },
+      "spec": {
+        "replicas": 1,
+        "selector": {
+          "matchLabels": {
+            "app.kubernetes.io/name": actor.name,
+            "app.kubernetes.io/instance": playbook.name_any()
+          }
+        },
+        "template": {
+          "metadata": {
+            "labels": {
+              "app.kubernetes.io/name": actor.name,
+              "app.kubernetes.io/instance": playbook.name_any()
+            }
+          },
+          "spec": {
+            "containers": [
+              {
+                "name": actor.name,
+                "image": actor.image,
+                "imagePullPolicy": "IfNotPresent",
+              }
+            ]
+          }
+        }
+      }
     }))
     .map_err(Error::SerializationError)?;
 
