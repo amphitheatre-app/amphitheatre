fee53f67d4aa321dfd701ba24c1f92e78f4a7f5f
Eguo Wang <wangeguo@gmail.com>
2025-01-18 20:17:28 +0800

    chore(deps): upgrade axum and amp-common & utoipa

--- apiserver/src/routes.rs
+++ apiserver/src/routes.rs
@@ -23,21 +23,21 @@ use crate::handlers;
 pub fn build() -> Router<Arc<Context>> {
     Router::new()
         // actors
-        .route("/v1/actors/:pid/:name", get(handlers::actor::detail))
-        .route("/v1/actors/:pid/:name/logs", get(handlers::actor::logs))
-        .route("/v1/actors/:pid/:name/info", get(handlers::actor::info))
-        .route("/v1/actors/:pid/:name/stats", get(handlers::actor::stats))
-        .route("/v1/actors/:pid/:name/sync", post(handlers::actor::sync))
+        .route("/v1/actors/{pid}/{name}", get(handlers::actor::detail))
+        .route("/v1/actors/{pid}/{name}/logs", get(handlers::actor::logs))
+        .route("/v1/actors/{pid}/{name}/info", get(handlers::actor::info))
+        .route("/v1/actors/{pid}/{name}/stats", get(handlers::actor::stats))
+        .route("/v1/actors/{pid}/{name}/sync", post(handlers::actor::sync))
         //
         // playbooks
         .route("/v1/playbooks", get(handlers::playbook::list))
         .route("/v1/playbooks", post(handlers::playbook::create))
-        .route("/v1/playbooks/:id", get(handlers::playbook::detail))
-        .route("/v1/playbooks/:id", patch(handlers::playbook::update))
-        .route("/v1/playbooks/:id", delete(handlers::playbook::delete))
+        .route("/v1/playbooks/{id}", get(handlers::playbook::detail))
+        .route("/v1/playbooks/{id}", patch(handlers::playbook::update))
+        .route("/v1/playbooks/{id}", delete(handlers::playbook::delete))
         //
-        .route("/v1/playbooks/:id/actions/start", post(handlers::playbook::start))
-        .route("/v1/playbooks/:id/actions/stop", post(handlers::playbook::stop))
-        .route("/v1/playbooks/:id/events", get(handlers::playbook::events))
-        .route("/v1/playbooks/:id/actors", get(handlers::actor::list))
+        .route("/v1/playbooks/{id}/actions/start", post(handlers::playbook::start))
+        .route("/v1/playbooks/{id}/actions/stop", post(handlers::playbook::stop))
+        .route("/v1/playbooks/{id}/events", get(handlers::playbook::events))
+        .route("/v1/playbooks/{id}/actors", get(handlers::actor::list))
 }
